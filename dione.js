9ca8dd861388dea46d6d7130e1715809d708cff8
Aksaa12 <gesersendi@gmail.com>
2024-10-24 01:20:37 +0700

    Update dione.js

--- dione.js
+++ dione.js
@@ -3,12 +3,20 @@ const fs = require('fs');
 
 // Membaca private key dari file
 const privateKey = fs.readFileSync('data.txt', 'utf8').trim();
+const walletAddress = "0xYourWalletAddress";  // Ganti dengan alamat wallet Anda
 
 // URL faucet untuk Dione Odyssey Testnet
-const faucetUrl = `https://testnet.odysseyscan.com/api/faucet?privateKey=${privateKey}&amount=2000`;
+const faucetUrl = 'https://testnet.odysseyscan.com/api/faucet';
+
+// Data yang akan dikirim
+const data = {
+    privateKey: privateKey,
+    address: walletAddress,
+    amount: 2000,  // Jumlah token yang diminta (2000 Dione)
+};
 
 // Melakukan permintaan ke faucet
-axios.get(faucetUrl)
+axios.post(faucetUrl, data)
     .then(response => {
         console.log("Permintaan berhasil:", response.data);
     })
